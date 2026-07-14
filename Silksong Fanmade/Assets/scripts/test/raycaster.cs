1f6f7abbec1c2d7ae77a0afc579f45d351464b72
Beecat21 <rodeiguezedgar90@gmail.com>
2026-07-14 13:01:10 -0500

    cambios en el raycast
    ahora el raycast aparece desde un objeto vacio hacia abajo

--- Silksong Fanmade/Assets/scripts/test/raycaster.cs
+++ Silksong Fanmade/Assets/scripts/test/raycaster.cs
@@ -4,26 +4,31 @@ using UnityEngine;
 
 public class raycaster : MonoBehaviour
 {
-    public float maxDistance = 50f;
-    public LayerMask targetLayer; // Filter which layers to hit
-    public Vector3 lastHit;
-     [SerializeField] Triggger particula;
+    public Transform topEmptyObject;
+    public float forwardOffset = 1.0f; // Distance in front of the object
+    public float rayLength = 5.0f;     // How far down the ray goes
+    public LayerMask targetLayer; // Filter which layers to hits
+    Triggger particula;
      [SerializeField] ParticleSystem impacto = null;
      public void Raywizard()
     {
         // 1. Define the origin and direction
-        Vector3 origin = transform.position;
-        Vector3 direction = transform.forward;
+        Vector3 origin = topEmptyObject.position + (transform.forward * forwardOffset);
+        Vector3 direction = transform.TransformDirection(Vector3.down);
 
         // 2. Variable to hold impact details
         RaycastHit hit;
-
+        if (TryGetComponent<Collider>(out Collider col))
+        {
+            origin.y += col.bounds.extents.y;
+        }
         // 3. Fire the raycast
-        if (Physics.Raycast(origin, direction, out hit, maxDistance, targetLayer))
+        if (Physics.Raycast(origin, direction, out hit, rayLength, targetLayer))
         {
             
-              impacto.transform.position = hit.point;
-            impacto.transform.rotation = Quaternion.LookRotation(hit.normal);
+            
+            impacto.transform.position = hit.point;
+            
 
             // 4. Play the particle system
             if (!impacto.isPlaying)
